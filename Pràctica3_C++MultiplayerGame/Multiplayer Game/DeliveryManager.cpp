c74b9277aeec3f56eeb75604d3da118003c617d8
Julià Mauri Costa <julimacowork@gmail.com>
2020-12-06 20:09:10 +0100

    DeliveryManager donete :doughnut:
    Co-Authored-By: Alexis Cosano <10940410+MrAlcoro@users.noreply.github.com>

--- Pràctica3_C++MultiplayerGame/Multiplayer Game/DeliveryManager.cpp
+++ Pràctica3_C++MultiplayerGame/Multiplayer Game/DeliveryManager.cpp
@@ -1,3 +1,105 @@
 #include "Networks.h"
+#include "DeliveryManager.h"
 
 // TODO(you): Reliability on top of UDP lab session
+
+void DeliveryReplication::onDeliverySuccess(DeliveryManager* deliveryManager) {
+	LOG("Delivery Success");
+}
+
+void DeliveryReplication::onDeliveryFailure(DeliveryManager* deliveryManager)
+{
+	LOG("Delivery failed, sending the packet again", NULL, LOG_TYPE_ERROR);
+}
+
+void DeliveryManager::processAckdSequenceNumbers(const InputMemoryStream& packet)
+{
+	while (packet.RemainingByteCount() > 0)
+	{
+		uint32 sequenceN;
+		packet >> sequenceN;
+
+		uint32 count = 0;
+		for (auto iter = pendingDeliveries.begin(); iter != pendingDeliveries.end(); iter++, count++) {
+			if (count == sequenceN)
+			{
+				iter->delegate->onDeliverySuccess(this);
+				pendingDeliveries.erase(iter);
+				break;
+			}
+		}
+	}
+}
+
+void DeliveryManager::processTiemdOutPackets()
+{
+	for (auto iter = pendingDeliveries.begin(); iter != pendingDeliveries.end(); iter++)
+	{
+		iter->dispatchTime += Time.deltaTime;
+		if(iter->dispatchTime > PACKET_DELIVERY_TIMEOUT_SECONDS)
+		{
+			iter->dispatchTime = 0.0;
+			iter->delegate->onDeliveryFailure(this);
+		}
+	}
+}
+
+bool DeliveryManager::processSequencerNumber(const InputMemoryStream& packet)
+{
+	bool ret = false;
+	uint32 totalSequenceN;
+	packet >> totalSequenceN;
+
+	for (uint32 i = 0; i < totalSequenceN; i++) {
+		uint32 sequenceNumber;
+		packet >> sequenceNumber;
+		if (next_expected_number == sequenceNumber)
+		{
+			sequence_numbers_pending_ack.push_back(sequenceNumber);
+			ret = true;
+		}
+	}
+	return ret;
+}
+
+void DeliveryManager::clear()
+{
+	pendingDeliveries.clear();
+	sequence_numbers_pending_ack.clear();
+}
+
+bool DeliveryManager::hasSequenceNumberPendingAck() const
+{
+	return !sequence_numbers_pending_ack.empty();
+}
+
+void DeliveryManager::writeSequenceNumbersPendingAck(OutputMemoryStream& packet)
+{
+	for (auto iter = sequence_numbers_pending_ack.begin(); iter != sequence_numbers_pending_ack.end(); iter++)
+		packet << *iter;
+	sequence_numbers_pending_ack.clear();
+}
+
+bool DeliveryManager::hasSequenceNumberPending() const
+{
+	return !pendingDeliveries.empty();
+}
+
+Delivery* DeliveryManager::createDelivery(OutputMemoryStream& packet)
+{
+	Delivery newDelivery;
+	newDelivery.sequenceNumber = next_sequence_number++;
+	newDelivery.delegate = static_cast<DeliveryDelegate*>(new DeliveryReplication(packet));
+	pendingDeliveries.push_back(newDelivery);
+	return &pendingDeliveries[newDelivery.sequenceNumber];
+}
+
+void DeliveryManager::writeAllSequenceNumber(OutputMemoryStream& packet)
+{
+	packet << pendingDeliveries.size();
+	for (auto iter = pendingDeliveries.begin(); iter != pendingDeliveries.end(); iter++)
+		packet << iter->sequenceNumber;
+
+	for (auto iter = pendingDeliveries.begin(); iter != pendingDeliveries.end(); iter++)
+		dynamic_cast<DeliveryReplication*>(iter->delegate)->Get(packet);
+}
--- Pràctica3_C++MultiplayerGame/Multiplayer Game/DeliveryManager.h
+++ Pràctica3_C++MultiplayerGame/Multiplayer Game/DeliveryManager.h
@@ -1,3 +1,68 @@
 #pragma once
 
 // TODO(you): Reliability on top of UDP lab session
+
+class DeliveryManager;
+
+class DeliveryDelegate
+{
+public:
+
+	virtual void onDeliverySuccess(DeliveryManager* deliveryManager) = 0;
+	virtual void onDeliveryFailure(DeliveryManager* deliveryManager) = 0;
+
+	virtual void Get(OutputMemoryStream& packet) = 0;
+};
+
+class DeliveryReplication : public DeliveryDelegate
+{
+public:
+	DeliveryReplication(OutputMemoryStream& packet) : deliveryPacket(packet) {}
+	~DeliveryReplication() {}
+
+	void onDeliverySuccess(DeliveryManager* deliveryManager)override;
+	void onDeliveryFailure(DeliveryManager* deliveryManager)override;
+
+	void Get(OutputMemoryStream& packet) override{
+		packet.Write(deliveryPacket.GetBufferPtr(), deliveryPacket.GetSize());
+	}
+
+private:
+	OutputMemoryStream deliveryPacket;
+};
+
+struct Delivery
+{
+	uint32 sequenceNumber = 0;
+	double dispatchTime = 0.0;
+	DeliveryDelegate* delegate = nullptr;
+};
+
+class DeliveryManager
+{
+public:
+
+	void clear();
+
+	//client
+	bool hasSequenceNumberPendingAck()const;
+	bool processSequencerNumber(const InputMemoryStream& packet);
+
+	void writeSequenceNumbersPendingAck(OutputMemoryStream& packet);
+
+	//server
+	bool hasSequenceNumberPending()const;
+	Delivery* createDelivery(OutputMemoryStream& packet);
+	void writeAllSequenceNumber(OutputMemoryStream& packet);
+	void processAckdSequenceNumbers(const InputMemoryStream& packet);
+	void processTiemdOutPackets();
+
+private:
+	//sender side
+	uint32 next_sequence_number = 0;
+	std::vector<Delivery> pendingDeliveries;
+
+	//recieiver side
+	uint32 next_expected_number = 0;
+	std::vector<uint32> sequence_numbers_pending_ack;
+};
